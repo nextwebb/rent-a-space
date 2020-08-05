eac75bc794f5140964c88f8174a42f8bafb1b928
nextwebb <oaikhenahpeterson@gmail.com>
2020-08-05 16:55:22 +0100

    enhancement: setup routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignIn';
+import {Route, BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+          <Route exact path="/signin" component={SignIn}/>
+          <Route exact path="/signup" component={SignUp}/>
+          <Route exact path="/home" component={App}/>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
