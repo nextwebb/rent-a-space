8baa3c7d50d3c2da61c496faaa44a36ac00455f1
nextwebb <oaikhenahpeterson@gmail.com>
2020-08-06 01:12:12 +0100

    sign in template

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 
 function SignIn () {
+
     return(
-        <div>
-            <h1>SignIn</h1>
-        </div>
+       <Container fluid>
+           <Row className="justify-content-md-center" >
+               <Col className="split-left"  sm md={4}  >
+
+               </Col>
+               <Col className="split-right "  xl sm md={8} >
+                <div style={{position:"relative", top:"20%", marginLeft:"10%"}}>
+                    <Col className="createTitle" style={{width:"200px"}}>
+                        <h2>Create your Account.</h2>
+                    </Col>
+
+                    <Col>
+                        <Form>
+                            <Row>
+                                <Col md="8">
+                                    <Form.Group controlId="formBasicEmail">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control size="lg" type="email" placeholder="Enter email" />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col md="8">
+                                    <Form.Group controlId="formBasicPassword" >
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control size="lg" type="password" placeholder="Password" />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md="8">
+                            <Button className="button" size="lg" type="submit" block style={{backgroundColor:"#1E1E64", border:"1px solid #1E1E64"}}>
+                                Sign Up
+                            </Button>
+                            </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </div>
+               </Col>
+           </Row>
+       </Container>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignIn';
 import {Route, BrowserRouter} from 'react-router-dom';
@@ -8,12 +9,13 @@ import * as serviceWorker from './serviceWorker';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-          <Route exact path="/signin" component={SignIn}/>
-          <Route exact path="/signup" component={SignUp}/>
-          <Route exact path="/home" component={App}/>
+      <Route exact path="/signin" component={SignIn}/>
+      <Route exact path="/signup" component={SignUp}/>
+      <Route exact path="/home" component={App}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
